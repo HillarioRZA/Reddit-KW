5047f108fb1bd6762fe9d4908692b42b9dc679a7
AlexanderRC01 <alexanderaldi01@gmail.com>
2025-01-23 21:20:20 +0700

    broken link fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        kUYAKIN PASTI BISA
-      </p>
-    </>
-  )
+    <div className="container">
+      <header>
+        <h1>HaiDok</h1>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <p>© 2024 Healthcare Inc. All Rights Reserved.</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
